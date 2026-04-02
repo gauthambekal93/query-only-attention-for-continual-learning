8c410c1ca1635511a3f5655355c2944a995d8fae
gauthambekal93 <gauthambekal93@gmail.com>
2026-04-02 11:00:14 -0500

    concat relu code

---
+++ algorithms/concat_relu/slowly_changing_regression/code_v1/neural_net.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 17 11:17:31 2025
+
+@author: gauthambekal93
+"""
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch import optim
+
+
+    
+class CReLU(nn.Module):
+    def forward(self, x):
+        return torch.cat([F.relu(x), F.relu(-x)], dim=1)
+
+class feed_forward_nn(nn.Module):
+    def __init__(self, input_size, num_features, num_outputs  ):
+        super(feed_forward_nn, self).__init__()
+        self.num_inputs = input_size
+        self.num_features = num_features
+        self.num_outputs = num_outputs
+        
+        self.fc1 = nn.Linear(input_size, num_features)
+        
+        self.fc2 = nn.Linear(num_features *2, num_outputs)
+        
+        nn.init.kaiming_uniform_(self.fc1.weight, nonlinearity='relu')
+        nn.init.zeros_(self.fc1.bias)
+        
+        nn.init.kaiming_uniform_(self.fc2.weight, nonlinearity='linear')
+        nn.init.zeros_(self.fc2.bias)
+        
+        self.crelu = CReLU()
+        
+        
+    def forward(self, x):
+        
+        x = self.crelu(self.fc1(x))
+        x = self.fc2(x)
+        
+        return x
+
+
+    
