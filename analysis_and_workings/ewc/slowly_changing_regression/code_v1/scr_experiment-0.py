d32bb41f7f43ff196dd75e8df9185b87e79c58e9
quasarsrus <apujari1@uncc.edu>
2026-04-29 18:20:54 -0400

    commit_v21.2

--- analysis_and_workings/ewc/slowly_changing_regression/code_v1/scr_experiment-0.py
+++ analysis_and_workings/ewc/slowly_changing_regression/code_v1/scr_experiment-0.py
@@ -48,7 +48,7 @@ def import_modules():
 class TrainContext:
     def __init__(self, num_inputs, num_features, num_outputs, hidden_activation, step_size, weight_decay, beta_1, beta_2):
         
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     
         self.net = feed_forward_nn(   num_inputs, num_features,num_outputs  )
         
